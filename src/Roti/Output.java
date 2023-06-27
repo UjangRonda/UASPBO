84292eacf1b30c0225ed2360b4b702da1158ffd8
Riyan Hidayat <riyanhidayat@student.uns.ac.id>
2023-06-27 23:23:48 +0700

    update

--- src/Roti/Output.java
+++ src/Roti/Output.java
@@ -22,48 +22,48 @@ public class Output {
         System.out.println("Harga per Roti Varian 3 : " + x + " Rupiah" );
     }
     static void printTepung (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Tepung : " + x + " Gram");
     }
     static void printGula (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Gula : " + x + " Gram");
     }
     static void printButter (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Butter : " + x + " Gram");
     }
     static void printRagi (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Ragi : " + x + " Gram");
     }
     static void printSusuBubuk (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Susu Bubuk : " + x + " Gram");
     }
     static void printSusuCair (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Susu Cair : " + x + " Gram");
     }
     static void printTelur (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Telur : " + x + " Gram");
     }
     static void printEsBatu (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Es Batu : " + x + " Gram");
     }
     static void printKeju (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Keju : " + x + " Gram");
     }
     static void printCoklat (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Coklat : " + x + " Gram");
     }
     static void printRedBean (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Selai Red Bean : " + x + " Gram");
     }
     static void printVanilla (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Cream Vanila : " + x + " Gram");
     }
     static void printSosis (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Sosis : " + x + " Gram");
     }
     static void printSmokedBeef (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Smoked Beef : " + x + " Gram");
     }
     static void printBawangBombay (int x){
-        System.out.println("Berat Bahan : " + x + " Gram");
+        System.out.println("Berat Bawang Bombay : " + x + " Gram");
     }
 }
